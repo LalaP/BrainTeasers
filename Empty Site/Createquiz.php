e329cba6726fb86ce757809944dc71882ea48420
kenetkhoo <kenetkhoo@gmail.com>
2013-11-09 04:07:01 -0800

    1

---
+++ Empty Site/Createquiz.php
@@ -0,0 +1,80 @@
+
+<head>
+
+<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+
+<meta name="description" content="" />
+
+<meta name="keywords" content="" />
+
+<meta name="author" content="" />
+
+<link rel="stylesheet" type="text/css" href="style.css" media="screen" />
+
+
+</head>
+
+	<body>
+
+		<div id="wrapper">
+
+<?php include('includes/header.php'); ?>
+
+<?php include('includes/nav.php'); ?>
+
+<div id="content">
+
+	<div id="form_container">
+	
+			<h2>Create Quiz</h2>
+		</div>						
+			<ul >
+			
+					<li id="li_1" >
+		<label class="description" for="element_1">Quiz Name </label>
+		<div>
+			<input id="element_1" name="element_1" class="element text medium" type="text" maxlength="255" value=""/> 
+		</div> 
+		</li>		<li id="li_3" >
+		<label class="description" for="element_3">Date Due </label>
+		<span>
+			<input id="element_3_1" name="element_3_1" class="element text" size="2" maxlength="2" value="" type="text"> /
+			<label for="element_3_1">MM</label>
+		</span>
+		<span>
+			<input id="element_3_2" name="element_3_2" class="element text" size="2" maxlength="2" value="" type="text"> /
+			<label for="element_3_2">DD</label>
+		</span>
+		<span>
+	 		<input id="element_3_3" name="element_3_3" class="element text" size="4" maxlength="4" value="" type="text">
+			<label for="element_3_3">YYYY</label>
+		</span>
+
+		</li>		<li id="li_2" >
+		<label class="description" for="element_2">Number of Questions </label>
+		<div>
+			<input id="element_2" name="element_2" class="element text small" type="text" maxlength="255" value=""/> 
+		</div> 
+		</li>		<li id="li_4" >
+		<label class="description" for="element_4">Generate Random Answers </label>
+		<div>
+		<select class="element select small" id="element_4" name="element_4"> 
+			<option value="" selected="selected"></option>
+<option value="1" >No</option>
+<option value="2" >Yes</option>
+
+	
+    dfdfdf
+</p>
+
+</div> <!-- end #content -->
+
+<?php include('includes/sidebar.php'); ?>
+
+<?php include('includes/footer.php'); ?>
+
+		</div> <!-- End #wrapper -->
+
+	</body>
+
+</html>
